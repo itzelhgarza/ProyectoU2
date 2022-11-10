3d6ead408d5b6d9c04dac27ee04b6ebdfbd5c5ce
itzelhgarza <itzel.garza216628@potros.itson.edu.mx>
2022-11-09 21:09:04 -0700

    Actualización

--- rutas/index.js
+++ rutas/index.js
@@ -1,9 +1,28 @@
 //En este arhivo estarán todos los llamados (casos de uso)
 const express = require('express');
+const estudiante = require('../modelos/estudiante');
 const rutas = express.Router();
 
 //importar modelo de la BD
 const Estud = require('../modelos/estudiante');
+
+//middleware
+rutas.use(function(req,res,next){
+    if(req.query._method == 'PUT'){
+        req.method = 'PUT';
+        req.url = req.path; 
+    } else if(req.query._method == 'DELETE'){
+        req.method = 'DELETE';
+        req.url = req.path;
+    }
+    next();
+});
+
+rutas.put('/estudiantes/:id', async(req, res, next)=>{
+    const id = req.params.id;
+    await Estud.updateOne({id:id}, {$set:{aprobado:true}});
+    res.redirect('/');
+});
 
 //endpoint default
 rutas.get('/',async(req,res)=>{
@@ -21,9 +40,18 @@ rutas.post('/estudiantes',async(req,res)=>{
     
 });
 
-//delete ****temporal****
-// rutas.get('/estudiantes/:id',(req,res)=>{
+//borrar estudiante
+rutas.delete('/estudiantes/:id', async(req,res)=>{
+    const id = req.params.id;
+    await Estud.deleteOne({id:id});
+    res.redirect('/');
+});
 
-// });
+//Obtener estudiante
+rutas.get('/estudiantes/:id',async(req,res)=>{
+    const id = req.params.id;
+    await Estud.findOne({id:id});
+    res.redirect('/');
+});
 
 module.exports=rutas;
