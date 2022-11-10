a7fffd2f0482d91dd1216c9ec3020ac203f1091c
itzelhgarza <itzel.garza216628@potros.itson.edu.mx>
2022-11-10 01:41:17 -0700

    Corrección en funciones

--- rutas/index.js
+++ rutas/index.js
@@ -7,51 +7,67 @@ const rutas = express.Router();
 const Estud = require('../modelos/estudiante');
 
 //middleware
-rutas.use(function(req,res,next){
-    if(req.query._method == 'PUT'){
+rutas.use(function (req, res, next) {
+    if (req.query._method == 'PUT') {
         req.method = 'PUT';
-        req.url = req.path; 
-    } else if(req.query._method == 'DELETE'){
+        req.url = req.path;
+    } else if (req.query._method == 'DELETE') {
         req.method = 'DELETE';
+        req.url = req.path;
+    } else if(req.query._method == 'GET'){
+        req.method = 'GET';
         req.url = req.path;
     }
     next();
 });
 
-rutas.put('/estudiantes/:id', async(req, res, next)=>{
+
+//Aprobado 
+rutas.put('/estudiantes/:id', async (req, res, next) => {
     const id = req.params.id;
-    await Estud.updateOne({id:id}, {$set:{aprobado:true}});
+    const estudiante = Estud.find({id:id});
+    let status = estudiante.aprobado;
+    console.log("STATUS ------"+status);
+    if(status == "false"){
+        await Estud.updateOne({ id: id }, { $set: { aprobado: true } });
+    }else{
+        await Estud.updateOne({ id: id }, { $set: { aprobado: false } });
+    }
     res.redirect('/');
 });
 
 //endpoint default
-rutas.get('/',async(req,res)=>{
+rutas.get('/', async (req, res) => {
     //res.send("Hola, desde la ruta GET/");
     const listaEstudiantes = await Estud.find();
     console.log(listaEstudiantes);
-    res.render("hola",{listaEstudiantes});
+    res.render("hola", { listaEstudiantes });
 });
 
 //insertar estudiante
-rutas.post('/estudiantes',async(req,res)=>{
+rutas.post('/estudiantes', async (req, res) => {
     var e = new Estud(req.body);
     await Estud.insertMany(e)
     res.redirect('/');
-    
+
 });
 
 //borrar estudiante
-rutas.delete('/estudiantes/:id', async(req,res)=>{
+rutas.delete('/estudiantes/:id', async (req, res) => {
     const id = req.params.id;
-    await Estud.deleteOne({id:id});
+    await Estud.deleteOne({ id: id });
     res.redirect('/');
 });
 
 //Obtener estudiante
-rutas.get('/estudiantes/:id',async(req,res)=>{
+rutas.get('/estudiantes/:id', async (req, res) => {
     const id = req.params.id;
-    await Estud.findOne({id:id});
-    res.redirect('/');
+    const listaEstudiantes = await Estud.find({ id: id });
+    if (id == "Empty") {
+        res.redirect("/");
+    } else {
+        res.render("hola", { listaEstudiantes });
+    }
 });
 
-module.exports=rutas;
+module.exports = rutas;
